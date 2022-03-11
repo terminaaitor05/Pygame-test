c0eb9a81a34f6bd9dbc30e165eb7bc20099768f7
Aitor <98980628+terminaaitor05@users.noreply.github.com>
2022-03-11 22:34:31 +0100

    Update main.py

--- jueguito-epico-para-nada-copiado-con-un-videotutorial-de-youtube-main/jueguito epico/main.py
+++ jueguito-epico-para-nada-copiado-con-un-videotutorial-de-youtube-main/jueguito epico/main.py
@@ -124,6 +124,7 @@ def main():
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
                 run = False
+                pygame.quit()
                 quit()
 
             if event.type == pygame.KEYDOWN:
